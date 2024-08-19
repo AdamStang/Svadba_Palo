dc4ede5c24d8a031f3b6dce9bdf380baec5ad88b
Adam Stang <Adam_Stang@swissre.com>
2024-08-19 09:44:11 +0200

    update map pointer

--- scripts/leafletScript.js
+++ scripts/leafletScript.js
@@ -1,8 +1,8 @@
-var map = L.map('map').setView([48.5031913, 17.481375], 15.9);
+var map = L.map('map').setView([48.27696522818639, 17.558955889137273], 16);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([48.502207, 17.482273]).addTo(map);
+var marker = L.marker([48.27696522818639, 17.558955889137273]).addTo(map);
